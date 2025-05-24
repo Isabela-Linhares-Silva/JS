39e14cead46e97bf8e15054a8bb90413781a9e82
Isabela-Linhares-Silva <isabelalinharessilva@gmail.com>
2025-05-24 10:57:51 -0300

    js inicio

---
+++ Aula01/script.js
@@ -0,0 +1,9 @@
+
+// Seleciona o elemneto HTML por meio do id 'mensagem'
+const mensagemElemento = document.getElementById('mensagem');
+
+// Modificando o texto do elemnto
+mensagemElemento.textContent = 'Olá, Mundo! Este é meu primeiro Hello World com JavaScript!';
+
+document.writeln("OI OI");
+
---
+++ Aula02/script.js
@@ -0,0 +1,7 @@
+let nome = 'Carolina';
+let idade = 22;
+let habilitada= true;
+
+const pi= 3.14159;
+alert(PI)
+
