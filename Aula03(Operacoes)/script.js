88a9e2c047c6df6857c12eacfc68108dc1601249
Isabela-Linhares-Silva <isabelalinharessilva@gmail.com>
2025-05-24 11:47:10 -0300

    atualização aula 5

--- Aula03(Operacoes)/script.js
+++ Aula03(Operacoes)/script.js
@@ -1,4 +1,10 @@
-let num1=2;
-let num2 = 3;
-let soma= num1+num2;
-alert(soma)
+// let num1=2;
+// let num2 = 3;
+// let soma= num1+num2;
+// alert(soma)
+
+//console.log(prompt("Digite um numero: "));
+
+console.log(2+3)
+console.log("3+2")
+console.log("Um belo dia hoje")
